cfc25e5ae4c67e0114ef12506de1ffcbaf3e4062
douglasmosher <doug.a.mosher@gmail.com>
2016-01-17 16:34:48 -0800

    Updated to place front and back images side/side
    Adjust framerate, bitrate and resolution to get max quality possible

--- bin/do_ffmpeg.sh
+++ bin/do_ffmpeg.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
-/home/root/bin/ffmpeg/ffmpeg -s 320x240 -f video4linux2 -i /dev/video0 -f mpeg1video \
--b 800k -r 30 http://127.0.0.1:8082
+/home/root/bin/ffmpeg/ffmpeg -s 640x400 -f video4linux2 -i /dev/video0 -i /dev/video1 \
+-filter_complex "nullsrc=size=2560x800 [base];\
+[0:v] setpts=PTS-STARTPTS, scale=1280x800 [left];\
+[1:v] setpts=PTS-STARTPTS, scale=1280x800 [right];\
+[base][left] overlay=shortest=1 [tmp1];\
+[tmp1][right] overlay=shortest=1:x=1280" -f mpeg1video \
+-b 100k -r 30 http://127.0.0.1:8082
 
