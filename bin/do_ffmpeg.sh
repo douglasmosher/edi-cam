91fe75dca13dd942f00159f5922c9ffec93b3585
douglasmosher <doug.a.mosher@gmail.com>
2016-01-17 18:09:55 -0800

    Update do_ffmpeg.sh

--- bin/do_ffmpeg.sh
+++ bin/do_ffmpeg.sh
@@ -1,11 +1,3 @@
 #!/bin/sh
 
-/home/root/bin/ffmpeg/ffmpeg -s 640x360 -f video4linux2 -i /dev/video0 -s 640x360 -f video4linux2 -i /dev/video1 \
--filter_complex "nullsrc=size=1280x360 [base];
-        [0:v] setpts=PTS-STARTPTS, scale=640x360 [left];
-        [1:v] setpts=PTS-STARTPTS, scale=640x360 [right];
-        [base][left] overlay=shortest=1 [tmp1]; 
-        [tmp1][right] overlay=shortest=1:x=640
-" 
--c:v libx264 output.mkv
-#-c:v mpeg1video -b 100k -r 30 http://127.0.0.1:8082
+/home/root/bin/ffmpeg/ffmpeg -s 640x360 -f video4linux2 -i /dev/video0 -s 640x360 -f video4linux2 -i /dev/video1 -filter_complex "nullsrc=size=1280x360 [base]; [0:v] setpts=PTS-STARTPTS, scale=640x360 [left]; [1:v] setpts=PTS-STARTPTS, scale=640x360 [right]; [base][left] overlay=shortest=1 [tmp1]; [tmp1][right] overlay=shortest=1:x=640" -c:v mpeg1video -b 100k -r 30 http://127.0.0.1:8082
