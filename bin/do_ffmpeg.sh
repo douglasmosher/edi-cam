28576805073e44782e89735ba0f4ce2ebf6123de
douglasmosher <doug.a.mosher@gmail.com>
2016-01-17 17:37:25 -0800

    Update do_ffmpeg.sh

--- bin/do_ffmpeg.sh
+++ bin/do_ffmpeg.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 
-/home/root/bin/ffmpeg/ffmpeg -s 640x400 -f video4linux2 -i /dev/video0 -s 640x400 -f video4linux2 -i /dev/video1 \
--filter_complex "nullsrc=size=2560x800 [base];
-        [0:v] setpts=PTS-STARTPTS, scale=1280x800 [left];
-        [1:v] setpts=PTS-STARTPTS, scale=1280x800 [right];
+/home/root/bin/ffmpeg/ffmpeg -s 640x360 -f video4linux2 -i /dev/video0 -s 640x360 -f video4linux2 -i /dev/video1 \
+-filter_complex "nullsrc=size=1280x360 [base];
+        [0:v] setpts=PTS-STARTPTS, scale=640x360 [left];
+        [1:v] setpts=PTS-STARTPTS, scale=640x360 [right];
         [base][left] overlay=shortest=1 [tmp1]; 
-        [tmp1][right] overlay=shortest=1:x=1280
+        [tmp1][right] overlay=shortest=1:x=640
 " 
--c:v http://127.0.0.1:8082
-#-f mpeg1video -b 100k -r 30 http://127.0.0.1:8082
-
+-c:v -f mpeg1video -b 100k -r 30 http://127.0.0.1:8082
