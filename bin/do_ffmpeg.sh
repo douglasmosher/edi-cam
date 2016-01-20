2587b90cb37d411a611a753667e287c4796cb031
douglasmosher <doug.a.mosher@gmail.com>
2016-01-20 14:04:26 -0800

    Update do_ffmpeg.sh

--- bin/do_ffmpeg.sh
+++ bin/do_ffmpeg.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-/home/root/bin/ffmpeg/ffmpeg -s 640x400 -f video4linux2 -i /dev/video0 -s 640x400 -f video4linux2 -i /dev/video1 -filter_complex "nullsrc=size=1280x400 [base]; [0:v] setpts=PTS-STARTPTS, scale=640x400 [left]; [1:v] setpts=PTS-STARTPTS, scale=640x400 [right]; [base][left] overlay=shortest=1 [tmp1]; [tmp1][right] overlay=shortest=1:x=640" -f mpeg1video \
--b:v 100k -r 30 http://127.0.0.1:8082
+/home/root/bin/ffmpeg/ffmpeg -s 1280x800 -f video4linux2 -i /dev/video0 -s 1280x800 -f video4linux2 -i /dev/video1 -filter_complex "nullsrc=size=2560x800 [base]; [0:v] setpts=PTS-STARTPTS, scale=1280x800 [left]; [1:v] setpts=PTS-STARTPTS, scale=1280x800 [right]; [base][left] overlay=shortest=1 [tmp1]; [tmp1][right] overlay=shortest=1:x=1280" -f mpeg1video \
+-b:v 200k -r 25 http://127.0.0.1:8082
 #/home/root/bin/ffmpeg/ffmpeg -s 640x360 -f video4linux2 -i /dev/video0 -s 640x360 -f video4linux2 -i /dev/video1 -filter_complex "nullsrc=size=1280x360 [base]; [0:v] setpts=PTS-STARTPTS, scale=640x360 [left]; [1:v] setpts=PTS-STARTPTS, scale=640x360 [right]; [base][left] overlay=shortest=1 [tmp1]; [tmp1][right] overlay=shortest=1:x=640" -c:v mpeg1video -b 100k -r 30 http://127.0.0.1:8082
