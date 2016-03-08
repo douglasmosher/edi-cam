ce24cbca519f32a3225d4d948a689ef8872c7514
douglasmosher <doug.a.mosher@gmail.com>
2016-03-07 18:59:21 -0800

    create websocket in camcontrol..

--- web/client/camcontrol.js
+++ web/client/camcontrol.js
@@ -1,5 +1,7 @@
 (function(window){ "use strict";
 
+	var wsUrl = 'ws://192.168.0.22:8084/';
+	var ws = new WebSocket(wsUrl);
 	// LED slider 
 	var sliderChange = window.sliderChange = function(sliderValue) {
 	    console.log("Slider changed: " + sliderValue);
