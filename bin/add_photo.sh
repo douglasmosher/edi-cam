698d5b9a41bbe488721720d50b0525b2f20590f5
douglasmosher <doug.a.mosher@gmail.com>
2016-03-17 19:13:25 -0700

    try this again

--- bin/add_photo.sh
+++ bin/add_photo.sh
@@ -1,11 +1,11 @@
 #Ammend the xml file with a path to the newly added photo
-echo "we here"
-printenv
+echo we here
+env
 
-$ sed '29 s/<!-- add photo here -->/\
+sed 's/<!-- add photo here -->/\
 <!-- add photo here -->\
 <scene name="my_house" title="My House" onstart="" lat="49.2611738" lng="-123.2138352" heading="0.0">\
 <view hlookat="-1" vlookat="11" fovtype="MFOV" fov="120" maxpixelzoom="2.0" fovmin="70" fovmax="140" limitview="auto"/>\
 <image>\
 	<sphere url="my_house.jpg"/>\
-</image>' tour.xml
+</image>/' tour.xml
