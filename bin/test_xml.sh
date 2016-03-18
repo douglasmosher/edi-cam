1326b89684b08f4663eb09cc7cb7ae12eac600fc
douglasmosher <doug.a.mosher@gmail.com>
2016-03-17 17:28:48 -0700

    fixed /

--- bin/test_xml.sh
+++ bin/test_xml.sh
@@ -1,9 +1,9 @@
 #Ammend the xml file with a path to the newly added photo
-
-sed '29 s/<!-- add photo here -->/\
+#sed '29 s/<!-- add photo here -->/\
+sed 's/<!-- add photo here -->/\
 <!-- add photo here -->\
 line 1\
 line 2\
 line 3\
 line 4\
-line 5' test.xml
+line 5/' test.xml
