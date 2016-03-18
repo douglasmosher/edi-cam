4ab02a9a9bc3c323bf23935d894eb25d205cbdd1
douglasmosher <doug.a.mosher@gmail.com>
2016-03-17 19:46:57 -0700

    fix error

--- web/server/server.js
+++ web/server/server.js
@@ -61,11 +61,7 @@ wsServer.on('connection', function(socket) {
       console.log('Control Type --> ' + control.type);
       console.log('Latitude --> ' + control.lat);
       console.log('Longitude --> ' + control.lon);
-      childProcess.exec('../../bin/add_photo.sh', {env: {file: pic.num + pic.type, lat: control.lat, lon: control.lon}}, function(err, stdout, stderr) {
-        if (err) { throw err; }
-        console.log('stdout:\n', stdout);
-        console.log('stderr:\n', stderr);
-      });
+      childProcess.exec('../../bin/add_photo.sh', {env: {file: pic.num + pic.type, lat: control.lat, lon: control.lon}};
     }
     else if(control.type == "VID"){
       console.log('Control Type --> ' + control.type);
