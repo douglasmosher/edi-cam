37f1212a1b99a3c85d50b445635d5d2385e17cac
douglasmosher <doug.a.mosher@gmail.com>
2016-03-17 19:20:34 -0700

    try this..

--- bin/add_photo.sh
+++ bin/add_photo.sh
@@ -2,10 +2,19 @@
 echo we here
 env
 
-sed 's/<!-- add photo here -->/\
+sed "s/<!-- add photo here -->/\
 <!-- add photo here -->\
-<scene name="my_house" title="My House" onstart="" lat="49.2611738" lng="-123.2138352" heading="0.0">\
-<view hlookat="-1" vlookat="11" fovtype="MFOV" fov="120" maxpixelzoom="2.0" fovmin="70" fovmax="140" limitview="auto"/>\
-<image>\
-	<sphere url="my_house.jpg"/>\
-</image>/' tour.xml
+line 1 env.type\
+line 2 env.num\
+line 3\
+line 4\
+line 5/" test.xml
+
+#sed 's/<!-- add photo here -->/\
+#<!-- add photo here -->\
+#<scene name="my_house" title="My House" onstart="" lat="49.2611738" lng="-123.2138352" heading="0.0">\
+#<view hlookat="-1" vlookat="11" fovtype="MFOV" fov="120" maxpixelzoom="2.0" fovmin="70" fovmax="140" limitview="auto"/>\
+#<image>\
+#	<sphere url="my_house.jpg"/>\
+#</image>/' tour.xml
+
