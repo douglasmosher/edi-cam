798cf4595cde8f8d708f271839ffc5b2afc1f482
douglasmosher <doug.a.mosher@gmail.com>
2016-03-17 19:55:48 -0700

    back

--- web/server/server.js
+++ web/server/server.js
@@ -62,7 +62,7 @@ wsServer.on('connection', function(socket) {
       console.log('Latitude --> ' + control.lat);
       console.log('Longitude --> ' + control.lon);
       childProcess.exec('../../bin/add_photo.sh', {env: {file: pic.num + pic.type, lat: control.lat, lon: control.lon}}, function(err, stdout, stderr) {
-        if (err)
+        if (err) { throw err; }
         console.log('stdout:\n', stdout);
         console.log('stderr:\n', stderr);
       });
