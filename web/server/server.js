eb18840c9a354e2f21d656fbf975687a1783682c
douglasmosher <doug.a.mosher@gmail.com>
2016-03-17 19:07:40 -0700

    add error messages

--- web/server/server.js
+++ web/server/server.js
@@ -61,7 +61,11 @@ wsServer.on('connection', function(socket) {
       console.log('Control Type --> ' + control.type);
       console.log('Latitude --> ' + control.lat);
       console.log('Longitude --> ' + control.lon);
-      childProcess.exec('../../bin/add_photo.sh', {env: {file: pic.num + pic.type, lat: control.lat, lon: control.lon}});
+      childProcess.exec('../../bin/add_photo.sh', {env: {file: pic.num + pic.type, lat: control.lat, lon: control.lon}}, function(err, stdout, stderr) {
+        if (err) { throw err; }
+        console.log('stdout:\n', stdout);
+        console.log('stderr:\n', stderr);
+      });
     }
     else if(control.type == "VID"){
       console.log('Control Type --> ' + control.type);
