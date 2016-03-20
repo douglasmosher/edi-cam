d62c8f4910e9bf9778dcd6c56ebb77eb5fc2ab34
douglasmosher <doug.a.mosher@gmail.com>
2016-03-19 19:20:48 -0700

    stream.kill

--- web/server/server.js
+++ web/server/server.js
@@ -62,7 +62,7 @@ wsServer.on('connection', function(socket) {
       console.log('Control Type --> ' + control.type);
       console.log('Latitude --> ' + control.lat);
       console.log('Longitude --> ' + control.lon);
-      child.kill(stream);
+      stream.kill(stream);
       //stream.kill();//'SIGQUIT');
       //childProcess.exec('../../bin/add_photo.sh', {env: {file: pic.num + pic.type, lat: control.lat, lon: control.lon}}, function(err, stdout, stderr) {
         //if (err) { throw err; }
