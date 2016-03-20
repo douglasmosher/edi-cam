0270bf7c63b082d2c28c1ac48f3a30ac59906e77
douglasmosher <doug.a.mosher@gmail.com>
2016-03-20 15:52:53 -0700

    added space

--- web/server/server.js
+++ web/server/server.js
@@ -75,7 +75,7 @@ wsServer.on('connection', function(socket) {
     }
     else if(control.type == "VID"){
       console.log('Control Type --> ' + control.type);
-      stream = childProcess.exec('/home/root/bin/ffmpeg/ffmpeg -s 160x120 -f video4linux2 -input_format mjpeg -i /dev/video0 -s 160x120 -f video4linux2 -input_format mjpeg -i /dev/video1 -filter_complex "nullsrc=size=320x120 [base]; [0:v] setpts=PTS-STARTPTS, scale=160x120 [left]; [1:v] setpts=PTS-STARTPTS, scale=160x120 [right]; [base][left] overlay=shortest=1 [tmp1]; [tmp1][right] overlay=shortest=1:x=160" -f mpeg1video -b:v 1000k\
+      stream = childProcess.exec('/home/root/bin/ffmpeg/ffmpeg -s 160x120 -f video4linux2 -input_format mjpeg -i /dev/video0 -s 160x120 -f video4linux2 -input_format mjpeg -i /dev/video1 -filter_complex "nullsrc=size=320x120 [base]; [0:v] setpts=PTS-STARTPTS, scale=160x120 [left]; [1:v] setpts=PTS-STARTPTS, scale=160x120 [right]; [base][left] overlay=shortest=1 [tmp1]; [tmp1][right] overlay=shortest=1:x=160" -f mpeg1video -b:v 1000k \
 http://127.0.0.1:8082'); 
       vid.num++;
     }
