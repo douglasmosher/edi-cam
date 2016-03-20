cda355c4043664a07ecc58771fac3c224f98d02e
douglasmosher <doug.a.mosher@gmail.com>
2016-03-19 21:34:56 -0700

    wait .5s

--- bin/add_photo.sh
+++ bin/add_photo.sh
@@ -5,7 +5,7 @@ sed -i 's/<!-- add photo here -->/\
 <view hlookat="-1" vlookat="11" fovtype="MFOV" fov="120" maxpixelzoom="2.0" fovmin="70" fovmax="140" limitview="auto"/>\
 <image>\
 	<sphere url="'photos/"$file"'"/>\
-</image>/' ../edi-cam/web/tour.xml
+</image>/' ../web/client/tour.xml
 
 #Take photo with ffmpeg
-/home/root/bin/ffmpeg/ffmpeg -s 1280x720 -f video4linux2 -input_format mjpeg -i /dev/video0 -s 1280x720 -f video4linux2 -input_format mjpeg -i /dev/video1 -filter_complex "nullsrc=size=2560x720 [base]; [0:v] setpts=PTS-STARTPTS, scale=1280x720 [left]; [1:v] setpts=PTS-STARTPTS, scale=1280x720 [right]; [base][left] overlay=shortest=1 [tmp1]; [tmp1][right] overlay=shortest=1:x=1280" -f image2 -vframes 1 /home/root/edi-cam/web/client/photos/"$file"
+/home/root/bin/ffmpeg/ffmpeg -thread_queue_size 512 -s 1280x720 -f video4linux2 -input_format mjpeg -i /dev/video0 -s 1280x720 -f video4linux2 -input_format mjpeg -i /dev/video1 -filter_complex "nullsrc=size=2560x720 [base]; [0:v] setpts=PTS-STARTPTS, scale=1280x720 [left]; [1:v] setpts=PTS-STARTPTS, scale=1280x720 [right]; [base][left] overlay=shortest=1 [tmp1]; [tmp1][right] overlay=shortest=1:x=1280" -thread_queue_size 512 -f image2 -vframes 1 /home/root/edi-cam/web/client/photos/"$file"
