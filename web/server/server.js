ead974db39bfb676dafde0bdd2c770fa410ff108
douglasmosher <doug.a.mosher@gmail.com>
2016-03-19 18:39:56 -0700

    i'm an ____

--- web/server/server.js
+++ web/server/server.js
@@ -64,7 +64,7 @@ wsServer.on('connection', function(socket) {
       console.log('Longitude --> ' + control.lon);
       stream.kill('SIGQUIT');
       //childProcess.exec('../../bin/add_photo.sh', {env: {file: pic.num + pic.type, lat: control.lat, lon: control.lon}}, function(err, stdout, stderr) {
-        if (err) { throw err; }
+        //if (err) { throw err; }
         //console.log('stdout:\n', stdout);
         //console.log('stderr:\n', stderr);
       //});
