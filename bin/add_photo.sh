40a3127d46d5feebce12a55f5cd7450237b37192
douglasmosher <doug.a.mosher@gmail.com>
2016-03-19 17:20:08 -0700

    Added folders for phot/videos

--- bin/add_photo.sh
+++ bin/add_photo.sh
@@ -7,4 +7,4 @@ sed -i 's/<!-- add photo here -->/\
 	<sphere url="'"$file"'"/>\
 </image>/' ../../bin/test.xml
 
-/home/root/bin/ffmpeg/ffmpeg -s 1280x720 -f video4linux2 -i /dev/video0 -s 1280x720 -f video4linux2 -i /dev/video1 -filter_complex "nullsrc=size=2560x720 [base]; [0:v] setpts=PTS-STARTPTS, scale=1280x720 [left]; [1:v] setpts=PTS-STARTPTS, scale=1280x720 [right]; [base][left] overlay=shortest=1 [tmp1]; [tmp1][right] overlay=shortest=1:x=1280" -f image2 "$file"
+/home/root/bin/ffmpeg/ffmpeg -s 1280x720 -f video4linux2 -input_format mjpeg -i /dev/video0 -s 1280x720 -f video4linux2 -input_format mjpeg -i /dev/video1 -filter_complex "nullsrc=size=2560x720 [base]; [0:v] setpts=PTS-STARTPTS, scale=1280x720 [left]; [1:v] setpts=PTS-STARTPTS, scale=1280x720 [right]; [base][left] overlay=shortest=1 [tmp1]; [tmp1][right] overlay=shortest=1:x=1280" -f image2 -vframes 1 /home/root/edi-cam/web/client/photos"$file"
