a36dff0108494cd3288f378460bb3e63fbdfc57f
douglasmosher <doug.a.mosher@gmail.com>
2016-03-26 15:32:56 -0700

    test sed

--- bin/test_photo.sh
+++ bin/test_photo.sh
@@ -1,7 +1,1 @@
-sed -i 's/<!-- add photo here -->/\
-<!-- add photo here -->\
-<scene name='69' title='69' onstart="" lat='69' lng='69'" heading="0.0">\
-<view hlookat="-1" vlookat="11" fovtype="MFOV" fov="120" maxpixelzoom="2.0" fovmin="70" fovmax="140" limitview="auto"/>\
-<image>\
-	<sphere url='69'/>\
-</image>/' test.xml
+sed -i 's/<!-- add photo here -->/blah/' test.xml
