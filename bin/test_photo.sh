7f4a53069456ecc173b264e22852aab205024ae4
douglasmosher <doug.a.mosher@gmail.com>
2016-03-26 15:50:54 -0700

    2 lines

--- bin/test_photo.sh
+++ bin/test_photo.sh
@@ -1,2 +1,3 @@
 sed -i 's/<!-- add photo here -->/\
-<!-- add photo here -->/' test.xml
+<!-- add photo here -->\
+<scene name="'"$file"'" title="'"$file"'" onstart="" lat="'"$lat"'" lng="'"$lon"'" heading="0.0">/' test.xml
