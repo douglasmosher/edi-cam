fe926e48639c23a2bfd3005e21d793b12aea8efa
douglasmosher <doug.a.mosher@gmail.com>
2016-03-26 15:26:44 -0700

    sed

--- bin/test_photo.sh
+++ bin/test_photo.sh
@@ -4,4 +4,4 @@ sed -i 's/<!-- add photo here -->/\
 <view hlookat="-1" vlookat="11" fovtype="MFOV" fov="120" maxpixelzoom="2.0" fovmin="70" fovmax="140" limitview="auto"/>\
 <image>\
 	<sphere url='69'/>\
-</image>/' /test.xml
+</image>/' test.xml
